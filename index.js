548c8dc9aff5c1f39bc7b6c52f70721ff76d2ed9
devanshu-gupta2002 <devanshuguptaknp@gmail.com>
2023-03-20 18:50:00 +0530

    initial commit

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ let notes = [
 //   response.send('<h1>Hello World!</h1>')
 // })
 
-app.get('/api/notes', (request, response) => {
+app.get('/', (request, response) => {
   response.json(notes)
 })
 
