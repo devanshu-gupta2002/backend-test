124e97a4b6c41d3e36d6bdac1d3c7f70c8732b1b
devanshu-gupta2002 <devanshuguptaknp@gmail.com>
2023-03-28 00:44:26 +0530

    verify

--- index.js
+++ index.js
@@ -8,24 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-// let notes = [
-//     {
-//       id: 1,
-//       content: "HTML is easy",
-//       important: true
-//     },
-//     {
-//       id: 2,
-//       content: "Browser can execute only JavaScript",
-//       important: false
-//     },
-//     {
-//       id: 3,
-//       content: "GET and POST are the most important methods of HTTP protocol",
-//       important: true
-//     }
-//   ]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -36,16 +18,22 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+// app.get('/api/notes/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     const note = notes.find(note => note.id === id)
     
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
+//     if (note) {
+//       response.json(note)
+//     } else {
+//       response.status(404).end()
+//     }
+//   })
+
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note=> {
+    response.json(note)
   })
+})
 
   app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -54,17 +42,17 @@ app.get('/api/notes/:id', (request, response) => {
     response.status(204).end()
   })
 
-const generateId = () => {
-  const maxId = notes.length>0 ?
-  Math.max(...notes.map(n=> n.id))
-  : 0
-  return maxId +1
-}
+// const generateId = () => {
+//   const maxId = notes.length>0 ?
+//   Math.max(...notes.map(n=> n.id))
+//   : 0
+//   return maxId +1
+// }
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
   
-  if(!body.content)
+  if(body.content===undefined)
   {
     return response.status(400).json(
       {
@@ -73,13 +61,13 @@ app.post('/api/notes', (request, response) => {
     )
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
-  notes=notes.concat(note)
-  response.json(note)
+  })
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 const PORT = process.env.PORT
